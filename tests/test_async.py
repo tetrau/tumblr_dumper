35d8fa6cd3717d535d1c1abeb4bfe3a6169a7f0b
tetrau <tetrau01@gmail.com>
2017-10-08 14:42:40 +0800

    new asynchronous tumblr dumper

---
+++ tests/test_async.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import logging
+import time
+import asyncio
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from tumblr_dumper.async import AsyncTumblrDumper
+
+api_key = os.getenv('api_key')
+logger = logging.getLogger('tumblr_dumper')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+count = 0
+
+
+async def main():
+    global count
+    tumblr_dumper = AsyncTumblrDumper('staff', api_key=api_key)
+    async for post in tumblr_dumper:
+        count += 1
+        print('fetch posts {:06}/{:06}'.format(count, tumblr_dumper.total_posts()))
+        time.sleep(0.05)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
+logger.debug('Finish')
--- tumblr_dumper/async.py
+++ tumblr_dumper/async.py
@@ -1,36 +1,12 @@
 import logging
-import requests
-import requests.exceptions
+import aiohttp
+import json
+
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 from .exceptions import *
 from .utility import *
-
-
-class NetworkIO:
-    """
-    Class controls the network io. It must have a get(self, url) method which return HTTP get
-    response in dict.
-    """
-
-    def __init__(self):
-        self.session = requests.session()
-
-    def get(self, url):
-        logger.debug('GET {}'.format(url))
-        try:
-            r = self.session.get(url).json()
-        except requests.exceptions.RequestException:
-            raise ConnectionException()
-        else:
-            if r['meta']['status'] != 200:
-                raise HTTPException(r['meta']['status'], r['meta']['msg'])
-            else:
-                return r
-
-
-
 
 
 class TumblrPost(QuickAccessDict):
@@ -39,52 +15,59 @@ class TumblrPost(QuickAccessDict):
     """
 
 
-class TumblrFetcher:
+class AsyncNetworkIO:
     """
-    Fetch posts from tumblr blog.
-    ------------mechanism------------
-    posts:    00 01 02 03 04 05 06 07 08 09
-    get:        |01 02 03|
-    # bloger delete an post
-    posts:    01 02 03 04 05 06 07 08 09
-    get:                 |05 06 07|
-    # oop! 04 is missing! so offset -= (total_post_now - total_posts_before)
-    posts:    01 02 03 04 05 06 07 08 09
-    get:              |04 05 06|
-    # everything moves on...
-    posts:    01 02 03 04 05 06 07 08 09
-    get:                       |07 08 09|
-    ------------mechanism------------
-    Just call .fetch() repeatedly, it will return lists of TumblrBlog (There may be duplication).
-    When done, exception NoPostException will raise.
+    Same like NetworkIO, but it's asynchronous.
+    """
+
+    def __init__(self):
+        self.session = aiohttp.ClientSession()
+
+    async def get(self, url):
+        logger.debug('GET {}'.format(url))
+        try:
+            r = await self.session.get(url, proxy='http://127.0.0.1:8118')
+            r = json.loads(await r.text())
+        except aiohttp.ClientError as e:
+            raise ConnectionException(e)
+        else:
+            if r['meta']['status'] != 200:
+                raise HTTPException(r['meta']['status'], r['meta']['msg'])
+            else:
+                return r
+
+
+class AsyncTumblrFetcher:
+    """
+    Fetch posts from tumblr blog. Just like TumblrFetcher, but it's asynchronous.
     """
     API_URL = 'https://api.tumblr.com/v2/blog/{blog}/posts?reblog_info=true&offset={offset}&api_key={api_key}'
 
     def __init__(self, blog_identifier, api_key):
         self.blog = blog_identifier
         self.api_key = api_key
-        self.network = NetworkIO()
+        self.network = AsyncNetworkIO()
         self.prev_result = None
         self.prev_offset = 0
 
-    def fetch(self):
+    async def fetch(self):
         def is_first_fetch():
             return self.prev_result is None
 
-        def do_first_fetch():
+        async def do_first_fetch():
             url = self.API_URL.format(blog=self.blog,
                                       offset=0,
                                       api_key=self.api_key)
-            result = self.network.get(url)
+            result = await self.network.get(url)
             result = QuickAccessDict(result)
             self.prev_result = result
             return [TumblrPost(r) for r in result.response.posts]
 
-        def do_fetch():
+        async def do_fetch():
             url = self.API_URL.format(blog=self.blog,
                                       offset=self.prev_offset + 20,
                                       api_key=self.api_key)
-            result = self.network.get(url)
+            result = await self.network.get(url)
             result = QuickAccessDict(result)
             self.prev_result = result
             if (result.response.blog.total_posts >=
@@ -98,42 +81,44 @@ class TumblrFetcher:
                 return self.fetch()
 
         if is_first_fetch():
-            result = do_first_fetch()
+            result = await do_first_fetch()
         else:
-            result = do_fetch()
+            result = await do_fetch()
 
         if not result:
             raise NoPostException()
         else:
             return result
 
-class TumblrDumper:
+
+class AsyncTumblrDumper:
     """
     The generator is used to dump all posts from a tumblr blog.
     It iterate all posts without duplication.
+    It's asynchronous.
     """
 
     def __init__(self, blog_identifier, api_key):
-        self.tumblr_fetcher = TumblrFetcher(blog_identifier, api_key)
+        self.tumblr_fetcher = AsyncTumblrFetcher(blog_identifier, api_key)
         self.buffer = UniqueQueue(key=lambda x: (x.id, x.timestamp))
 
-    def __iter__(self):
+    async def __aiter__(self):
         return self
 
-    def reload(self):
-        result = self.tumblr_fetcher.fetch()
+    async def reload(self):
+        result = await self.tumblr_fetcher.fetch()
         self.buffer.push_many(result)
 
     def total_posts(self):
         return self.tumblr_fetcher.prev_result.response.blog.total_posts
 
-    def __next__(self):
+    async def __anext__(self):
         if len(self.buffer) > 0:
             return self.buffer.get()
         else:
             try:
-                self.reload()
+                await self.reload()
             except NoPostException:
-                raise StopIteration()
+                raise StopAsyncIteration()
             else:
                 return self.buffer.get()
